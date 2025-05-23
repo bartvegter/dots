44b8618d10be69374eac8a268169d02263b4f37e
Bart Vegter <accounts@bartvegter.com>
2025-05-23 15:06:36 +0200

    Added zen browser as new default browser

--- install.sh
+++ install.sh
@@ -23,7 +23,8 @@ USER_PKGS=(
   bat bottles brave-bin btop discord easyeffects fd file-roller firefox fzf lazygit lua-language-server
   nautilus nautilus-image-converter neovim obsidian oh-my-posh-bin onlyoffice-bin proton-vpn-gtk-app
   ripgrep seahorse spotify-launcher syncthing ticktick timeshift tldr udiskie viewnior vim
-  webp-pixbuf-loader zathura zathura-pdf-poppler zsh zsh-autosuggestions zsh-syntax-highlighting
+  webp-pixbuf-loader zathura zathura-pdf-poppler zen-browser-bin zsh zsh-autosuggestions
+  zsh-syntax-highlighting
 )
 
 GAMING_PKGS=(
