a46a2940eec445866bfa024b0b1a79ef7078fc4a
Bart Vegter <517242@student.saxion.nl>
2026-08-21 16:28:51 +0200

    Updated some styling and cleaned up config

--- .config/hypr/modules/autostart.lua
+++ .config/hypr/modules/autostart.lua
@@ -2,20 +2,25 @@ local launchApp = function(app)
 	hl.exec_cmd("uwsm app -- " .. app)
 end
 
+local launchAppDelayed = function(app)
+	hl.exec_cmd("while ! pgrep -x noctalia > /dev/null; do sleep 2; done; " .. "uwsm app -- " .. app)
+end
+
 local exec = function(cmd)
 	hl.exec_cmd(cmd)
 end
 
 hl.on("hyprland.start", function()
-	launchApp("vesktop")
 	launchApp("which kanshi && kanshi")
-	launchApp("which netbird-ui && netbird-ui -daemon-addr 'unix:///var/run/netbird/main.sock'")
 	launchApp("noctalia")
-	launchApp("sone")
+	launchApp("syncthingtray-qt6 --wait")
 	launchApp("udiskie")
 
+	launchAppDelayed("sone")
+	launchAppDelayed("vesktop")
+	launchAppDelayed("which netbird-ui && netbird-ui -daemon-addr 'unix:///var/run/netbird/main.sock'")
+
 	exec("systemctl --user start hyprpolkitagent.service")
-	exec("systemctl --user start syncthing.service")
-	exec("gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'") -- for GTK3 apps
-	exec("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'") -- for GTK4 apps
+	exec("gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'")
+	exec("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'")
 end)
--- .config/hypr/noctalia.lua
+++ .config/hypr/noctalia.lua
@@ -2,8 +2,12 @@
 
 local primary = "rgb(ffd866)"
 local surface = "rgb(2d2a2e)"
+local on_surface = "rgb(fcfcfa)"
 local secondary = "rgb(a9dc76)"
+local on_secondary = "rgb(2d2a2e)"
 local error = "rgb(ff6188)"
+local on_error = "rgb(2d2a2e)"
+local shadow = "rgb(2d2a2e)"
 
 local function apply_theme()
     hl.config({
@@ -13,7 +17,14 @@ local function apply_theme()
                 inactive_border = surface,
             },
         },
-
+        decoration = {
+            shadow = {
+                color = shadow,
+            },
+            glow = {
+                color = shadow,
+            },
+        },
         group = {
             col = {
                 border_active = secondary,
@@ -23,12 +34,17 @@ local function apply_theme()
             },
 
             groupbar = {
+                gradients = true,
                 col = {
                     active = secondary,
                     inactive = surface,
                     locked_active = error,
                     locked_inactive = surface,
                 },
+                text_color = on_secondary,
+                text_color_inactive = on_surface,
+                text_color_locked_active = on_error,
+                text_color_locked_inactive = on_surface,
             },
         },
     })
@@ -38,8 +54,12 @@ return {
     colors = {
         primary = primary,
         surface = surface,
+        on_surface = on_surface,
         secondary = secondary,
+        on_secondary = on_secondary,
         error = error,
+        on_error = on_error,
+        shadow = shadow,
     },
     apply_theme = apply_theme
 }
