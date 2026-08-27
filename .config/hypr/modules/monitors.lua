0763aa853fdebbe6631552423c5acaf191c19a2e
Bart Vegter <517242@student.saxion.nl>
2026-08-27 21:44:34 +0200

    Added new default wallpaper

--- .config/hypr/modules/monitors.lua
+++ .config/hypr/modules/monitors.lua
@@ -1,4 +1,5 @@
 hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "auto" })
 hl.monitor({ output = "DP-3", mode = "2560x1440@165", position = "0x0", scale = 1, vrr = 2 })
 hl.monitor({ output = "eDP-1", mode = "2560x1600@90", position = "0x0", scale = 1.333333 })
+hl.monitor({ output = "DP-2", mode = "2560x1440@165", position = "auto-center-up", scale = 1 })
 hl.monitor({ output = "HDMI-A-1", mode = "2560x1440@120", position = "auto-center-up", scale = 1 })
