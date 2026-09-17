e97a5982f4e56ddcae48d61571c530ac589a99ba
Bart Vegter <517242@student.saxion.nl>
2026-09-17 14:52:30 +0200

    Updated monitor setup

--- .config/hypr/modules/monitors.lua
+++ .config/hypr/modules/monitors.lua
@@ -1,20 +1,58 @@
--- defaults
-hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "auto" })
+local function setup_monitors()
+	local monitors = {}
+	for _, m in ipairs(hl.get_monitors()) do
+		monitors[m.name] = true
+	end
 
--- home desktop
-hl.monitor({ output = "DP-3", mode = "2560x1440@165", position = "0x0", scale = 1, vrr = 2 })
+	-- defaults for unknown monitors
+	hl.monitor({
+		output = "",
+		mode = "preferred",
+		position = "auto",
+		scale = "auto",
+	})
 
--- laptop internal display
-hl.monitor({ output = "eDP-1", mode = "2560x1600@90", position = "0x0", scale = 1.333333 })
+	-- laptop internal display
+	hl.monitor({
+		output = "eDP-1",
+		mode = "2560x1600@90",
+		position = "0x0",
+		scale = 1.333333,
+	})
 
--- home office monitor
--- hl.monitor({ output = "DP-2", mode = "2560x1440@165", position = "auto-center-up", scale = 1 })
--- hl.monitor({ output = "HDMI-A-1", mode = "2560x1440@120", position = "auto-center-up", scale = 1 })
+	-- office monitors
+	hl.monitor({
+		output = "desc: LG Electronics BK550Y 012NTBK8G957",
+		mode = "1920x1080@60",
+		position = "-960x-1080",
+	})
+	hl.monitor({
+		output = "desc: Samsung Electric Company LF24T450F HK7X400732",
+		mode = "1920x1080@75",
+		position = "960x-1080",
+	})
 
--- office monitors
-hl.monitor({ output = "desc: LG Electronics BK550Y 012NTBK8G957", mode = "1920x1080@60", position = "-960x-1080" })
-hl.monitor({
-	output = "desc: Samsung Electric Company LF24T450F HK7X400732",
-	mode = "1920x1080@75",
-	position = "960x-1080",
-})
+	-- home office monitor setup based on connected laptop
+	if monitors["eDP-1"] then
+		-- home office second monitor mode (laptop)
+		hl.monitor({
+			output = "desc: Microstep MSI MAG274QRF CA8A201200443",
+			mode = "2560x1440@120",
+			position = "auto-center-up",
+		})
+	else
+		-- home office single monitor mode (desktop)
+		hl.monitor({
+			output = "desc: Microstep MSI MAG274QRF CA8A201200443",
+			mode = "2560x1440@165",
+			position = "0x0",
+			scale = 1,
+			vrr = 2,
+		})
+	end
+end
+
+setup_monitors()
+hl.on("monitor.added", setup_monitors)
+hl.on("monitor.removed", setup_monitors)
+hl.on("config.reloaded", setup_monitors)
